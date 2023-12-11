a4924aa4163f41861aa47e26f9e0bf1c34d77558
VedantLaheriInventyv <153286389+VedantLaheriInventyv@users.noreply.github.com>
2023-12-11 17:37:55 +0530

    Delete sumofdigits.py

--- sumofdigits.py
+++
@@ -1,6 +0,0 @@
-#program to print sum of digits
-number = int(input("Enter the Number: "))
-sum = 0
-for value in range(0, number + 1):
-    sum = sum + value
-print("sum of digits of an given number",sum)
