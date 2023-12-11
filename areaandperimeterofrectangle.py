9acaa13ca24e8b4c54a21a22f1935505a30ba6fd
VedantLaheriInventyv <153286389+VedantLaheriInventyv@users.noreply.github.com>
2023-12-11 17:29:51 +0530

    Delete areaandperimeterofrectangle.py

--- areaandperimeterofrectangle.py
+++
@@ -1,9 +0,0 @@
-#python program for calculating area and perimiter of rectangle
-l = int(input("Enter the length: "))
-b = int(input("Enter the breadth: "))
-
-area = l * b
-print("Area of Rectangle:", area)
-
-perimeter = 2 * (l + b)
-print("Perimeter of Rectangle:", perimeter)
