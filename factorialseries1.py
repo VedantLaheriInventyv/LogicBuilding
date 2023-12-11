1fc27a8e8ab7ed262c16712617afa86b23a5a626
VedantLaheriInventyv <153286389+VedantLaheriInventyv@users.noreply.github.com>
2023-12-11 17:34:58 +0530

    Delete factorialseries1.py

--- factorialseries1.py
+++
@@ -1,7 +0,0 @@
-#python program for factorial series  1! 3! 5! ...n=3
-n = int(input("Enter n: "))
-for i in range(1, n*2+1, 2):
-    ans = 1
-    for j in range(1, i+1):
-        ans = ans*j
-    print(ans, end=" ")
